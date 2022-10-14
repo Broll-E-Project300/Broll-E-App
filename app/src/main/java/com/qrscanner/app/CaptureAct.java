3481e350b37b173eeb9e440eff6829e7d7879036
Steven Kelly <stevenkelly418@gmail.com>
2022-10-14 14:28:13 +0100

    Initial project upload
    Added the initial Android studio project to the repo.
    This is a base project and already contains a QR code reader.

---
+++ app/src/main/java/com/qrscanner/app/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.qrscanner.app;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
---
+++ app/src/main/java/com/qrscanner/app/MainActivity.java
@@ -0,0 +1,52 @@
+package com.qrscanner.app;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonScan = findViewById(R.id.buttonScan);
+        buttonScan.setOnClickListener(view -> {
+            scanCode();
+        });
+    }
+
+    private void scanCode()
+    {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to turn flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->{
+        if(result.getContents() != null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+
+                }
+            }).show();
+        }
+    });
+}
