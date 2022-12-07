b83b9fd044c86a3181ac577c0f6d73affad0b4e1
rudgerylopes94 <63984559+rudgerylopes94@users.noreply.github.com>
2022-12-07 14:41:13 +0000

    updated screens, added activities

--- app/src/main/java/com/qrscanner/app/MainActivity.java
+++ app/src/main/java/com/qrscanner/app/MainActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     Button buttonScan;
-
+    private Button button, button2, button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,30 @@ public class MainActivity extends AppCompatActivity {
         buttonScan.setOnClickListener(view -> {
             scanCode();
         });
+        button = (Button) findViewById(R.id.btnPlans);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPlansActivity();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.btnCreditCard);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCreditCard();
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.btnMap);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMap();
+            }
+        });
+
     }
 
     private void scanCode()
@@ -49,4 +77,18 @@ public class MainActivity extends AppCompatActivity {
             }).show();
         }
     });
+
+
+    public void openPlansActivity(){
+        Intent intent = new Intent(this,Plans.class);
+        startActivity(intent);
+    }
+    public void openCreditCard(){
+        Intent intent = new Intent(this, CreditCard.class);
+        startActivity(intent);
+    }
+    public void openMap(){
+        Intent intent = new Intent(this, Map.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/qrscanner/app/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.qrscanner.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        },10000);
+
+    }
+}
