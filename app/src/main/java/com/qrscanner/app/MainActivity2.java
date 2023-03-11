3062106c0cef4bec11177a53c4f723e0cc96928c
ReginaldUG <S00214390@mail.itsligo.ie>
2023-03-11 18:53:16 +0000

    New activities

---
+++ app/src/main/java/com/qrscanner/app/MainActivity2.java
@@ -0,0 +1,227 @@
+package com.qrscanner.app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.location.LocationRequest;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private ImageButton menuBtn;
+    private Button scan_btn,getUmb;
+    private ImageView dialogbackBtn;
+    boolean check = false;
+
+    private ItemViewModel viewModel;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    FirebaseAuth firebaseAuth;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    LocationRequest locationRequest;
+    private final static int REQUEST_CODE = 101;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        scan_btn = findViewById(R.id.Scan_btn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user==null){
+            startActivity(new Intent(getApplicationContext(), Screen_LoginRegister.class));
+            finish();
+        }else{
+            while(check!=true){
+                if (ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION
+                            }, REQUEST_CODE);
+
+                }
+                if(ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+
+                    check=true;
+
+                }
+            }
+
+        }
+
+        //Location perission check
+
+        gso= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+
+        gsc= GoogleSignIn.getClient(this,gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //Initialize Map fragment
+        Fragment mapf = new MapFragment();
+        //Open fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, mapf)
+                .commit();
+
+        final  DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        menuBtn = findViewById(R.id.menu_btn);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawer.openDrawer(Gravity.LEFT);
+                TextView nav_email = findViewById(R.id.nav_useremail);
+                nav_email.setText(user.getEmail().toString());
+
+            }
+        });
+
+
+
+        //VIEW MODEL LOGIC
+        //Checks ItemViewModel class for change in variable to know when button was clicked
+        //This will assist in detecting when an umbrella icon was selected and open the bottomDialog
+      /*  viewModel = new ViewModelProvider(this).get(ItemViewModel.class);
+
+        viewModel.getSelectedItem().observe(this, item->{
+            showDockDialog();
+        });*/
+
+        //NavView LOGIC
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                boolean reply = false;
+                if (menuItem.getItemId()==R.id.nav_logout){
+
+                    logoutMenu(MainActivity2.this);
+
+                    reply= true;
+                }
+                if (menuItem.getItemId()==R.id.nav_plans){
+                    openplans(MainActivity2.this);
+                    reply = true;
+                }
+                return reply;
+            }
+
+
+        });
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FinishPayment.class));
+            }
+        });
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();*/
+
+
+    }
+
+    private void openplans(MainActivity2 mainActivity2) {
+        startActivity(new Intent(getApplicationContext(), Plans.class));
+    }
+
+    private void logoutMenu(MainActivity2 mainActivity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to logout? ");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        firebaseAuth.signOut();
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), Screen_LoginRegister.class));
+                    }
+                });
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+
+    //View model works perfectly though
+    /*private void showDockDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dockbottom_sheet);
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DockbottomAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+    }*/
+
+    //Closes nav drawer if back button is pressed. Instead of exiting the activity completely
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+
+    }
+}
